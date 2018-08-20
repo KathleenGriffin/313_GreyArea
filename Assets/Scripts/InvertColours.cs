3b515da9b9cada823f5cf230928738c58c0cdf91
Kathleen Griffin <kathleen@Kathleens-MacBook-Pro.local>
2018-08-21 01:37:23 +1200

    Added disco powerup.. sort of

--- Assets/Scripts/InvertColours.cs
+++ Assets/Scripts/InvertColours.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class InvertColours : MonoBehaviour {
 
+    Renderer rend;
+
 	// Use this for initialization
 	void Start () {
-		
+        rend = gameObject.GetComponent<Renderer>();
+        rend.enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(0, 0, 80 * Time.deltaTime);
+        transform.Rotate(0, 0, 300 * Time.deltaTime);
+
+
+
+        //a cheat way to make the powerup appear for the video hehehe
+        if(Input.GetKeyDown(KeyCode.P)) {
+            rend.enabled = true;
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            rend.enabled = false;
+        }
+
 	}
 
     //when someone picks up the poweruppppp!
---
+++ Assets/Scripts/Shadows.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shadows : MonoBehaviour {
+    Renderer rend;
+    bool on = true;
+    bool discoTime = false;
+	// Use this for initialization
+	void Start () {
+        rend = gameObject.GetComponent<Renderer>();
+        rend.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            discoTime = true;
+        }
+
+        if (discoTime == true) {
+            doDisco();
+        }
+
+		
+	}
+
+    //a power up
+    void doDisco() {
+        //Debug.Log("doing disco");
+        if ((Time.frameCount % 10) == 0) {
+            if (on == true) {
+                rend.enabled = false;
+                on = false;
+            }
+            else {
+                rend.enabled = true;
+                on = true;
+            }
+
+
+        }
+        
+    }
+
+}
